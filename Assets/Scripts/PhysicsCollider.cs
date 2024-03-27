ea7e31e49e192847c6ef222f2f2b869cf620cb22
JohnBeck19 <jbeck@student.neumont.edu>
2024-03-27 11:55:35 -0600

    Colliders and Rigidbody

--- Assets/Scripts/PhysicsCollider.cs
+++ Assets/Scripts/PhysicsCollider.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class PhysicsCollider : MonoBehaviour
 {
     string status;
+    Vector3 contact;
+    Vector3 normal;
+    [SerializeField] GameObject fx;
 
     private void OnCollisionEnter(Collision collision)
     {
         status = "Collision enter " + collision.gameObject.name;
+
+        contact = collision.contacts[0].point;
+        normal = collision.GetContact(0).normal;
+
+        if (fx) Instantiate(fx,contact,Quaternion.LookRotation(normal));
     }
     private void OnCollisionStay(Collision collision)
     {
-        
+        status = "Collision stay " + collision.gameObject.name;
+
     }
     private void OnCollisionExit(Collision collision)
     {
-        
+        status = "Collision exit " + collision.gameObject.name;
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -24,11 +34,13 @@ public class PhysicsCollider : MonoBehaviour
     }
     private void OnTriggerStay(Collider other)
     {
-        
+        status = "Trigger stay " + other.gameObject.name;
+
     }
     private void OnTriggerExit(Collider other)
     {
-        
+        status = "Trigger exit " + other.gameObject.name;
+
     }
 
     private void OnGUI()
@@ -36,5 +48,11 @@ public class PhysicsCollider : MonoBehaviour
         GUI.skin.label.fontSize = 16;
         Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
         GUI.Label(new Rect(screen.x,Screen.height-screen.y,250,70),status);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(contact, 0.2f);
+        Gizmos.DrawLine(contact, contact + normal);
     }
 }
