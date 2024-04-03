ca1062e9df1a6b8febc848639a312959988c9817
JohnBeck19 <jbeck@student.neumont.edu>
2024-04-03 11:59:02 -0600

    Block Buster

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ammo : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed = 1;
+    [SerializeField] float lifespan = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lifespan > 0) Destroy(gameObject,lifespan);
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddRelativeForce(Vector3.forward * speed,ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
